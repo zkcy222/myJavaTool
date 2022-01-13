4d93b7d195049712a2005e56ac74276dd2250d56
zkcy <745430646@qq.com>
2022-01-13 15:01:51 +0800

    1

---
+++ test/src/Main.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(SocketConnection.getURLSource("https://mp.weixin.qq.com/s/Y8uBfPC9Pp7Ss2UCfs8BgQ"));
+    }
+}
